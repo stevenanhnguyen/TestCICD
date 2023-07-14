321b47eb48713edc548daa55afe4f91116a5ae80
Anh Tú <at.mrsteven@gmail.com>
2023-07-14 11:03:20 +0700

    update

--- TestCICD/ViewController.swift
+++ TestCICD/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let test: String = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
